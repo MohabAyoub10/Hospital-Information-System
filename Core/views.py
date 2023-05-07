5520a2223907c74e32657c4e7c384be35eaabe03
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-05-07 07:24:09 +0300

    add user view set

--- Core/views.py
+++ Core/views.py
@@ -1,8 +1,14 @@
 from .models import *
 from rest_framework.viewsets import ModelViewSet
 from .serializer import *
+from djoser.views import UserViewSet as BaseUserViewSet
+from djoser.conf import settings
 # Create your views here.
-
-class UserViewSet(ModelViewSet):
-    queryset = User.objects.select_related('user_patient').all()
-    serializer_class = UserCreateSerializer
+class UserViewSet(BaseUserViewSet):
+    http_method_names = ('post','put')
+    def get_serializer_class(self):
+        if self.request.user.is_staff and self.action == 'create':
+            return AdminUserCreateSerializer
+        elif self.request.user.role == 'receptionist' and self.action == 'create':
+            return UserCreateSerializer
+        return settings.SERIALIZERS.user
