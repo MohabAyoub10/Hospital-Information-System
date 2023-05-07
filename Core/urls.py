969c3512719ac4bf091b58c1ee3aa8aef293b39d
Mahmoud <mahmouddabdelfattah72@gmail.com>
2023-05-07 07:16:31 +0300

    add register url

--- Core/urls.py
+++ Core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from Core.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView 
+from rest_framework.routers import DefaultRouter
+from .views import*
+
+router = DefaultRouter()
+router.register('register',UserViewSet,basename='register')
 
 urlpatterns = [
     path ('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
+urlpatterns += router.urls
