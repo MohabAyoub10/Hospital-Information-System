0ccfb8d343f08bd1df64563406912ea695e78d97
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-07 09:19:55 +0300

    add Pharmacy main models

--- Pharmacy/models.py
+++ Pharmacy/models.py
@@ -1,60 +1,52 @@
 from django.db import models
 from django.conf import settings
-from Hospital.models import Patient,Doctor
-from Appointments.models import BookedAppointment
-from datetime import datetime
+# from Hospital.models import Patient,Doctor
+# from Appointments.models import BookedAppointment
 # Create your models here.
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
 
-class Drug(models.Model):
+class Prescription(BaseModel):
+    doctor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='doctorr')
+    patient = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='patientt')
+    # appointment = models.ForeignKey(BookedAppointment,on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    notes = models.TextField()
+    dispensed_by = models.ForeignKey('Pharmacist',on_delete=models.CASCADE, null=True, blank=True, related_name='dispensed_by')
+    dispensed_confirm = models.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.patient} - {self.date}'
 
-    name = models.CharField(max_length=50)
-    drug_form = models.CharField(max_length=50)
-    brand_name = models.CharField(max_length=50)
-    company = models.CharField(max_length=50)
-    description = models.CharField(max_length=100, null=True)
-    stock_level = models.PositiveIntegerField(default=0)
-    expiry_date = models.DateField()
-    price = models.DecimalField(max_digits=15, decimal_places=2)
+
+class Drug(BaseModel):
+    name = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    expire_date = models.DateField()
+    form = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['id']
+
+class Pharmacist(BaseModel):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='pharmacist')
+    license = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name + ' ' + self.brand_name
-    
-
-class CurrentMedication(models.Model):
-    
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE,related_name='patient_medication')
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE,related_name='doctor_request')
-    appointment = models.ForeignKey(BookedAppointment, on_delete=models.CASCADE,related_name='appointment')
-
-
-
-class DrugRequestDetail(models.Model):
-
-    medication = models.ForeignKey(CurrentMedication, on_delete=models.CASCADE,related_name='medication_detail')
-    drug = models.ForeignKey(Drug, on_delete=models.CASCADE,related_name='drug_detail')
-    frquency = models.CharField(max_length=50)
-    duration = models.PositiveIntegerField()
-    dispense = models.BooleanField(default=False)
-
-
-
-
-class Pharmacist(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    Pharmacist_license = models.CharField(max_length=50)
-
-
-
-
-class Dispensing(models.Model):
-
-    medication = models.ForeignKey(CurrentMedication, on_delete=models.CASCADE,related_name='medication_dispense')
-    Pharmacist = models.ForeignKey(Pharmacist, on_delete=models.CASCADE,related_name='dispenser')
-    time_date = models.DateTimeField()
-
-
-
-class DispensingDetails(models.Model):
-    dispensing = models.ForeignKey(Dispensing, on_delete=models.CASCADE,related_name='dispensing_detail')
-    drug = models.ForeignKey(Drug, on_delete=models.CASCADE,related_name='drug_dispense')
+        return f'{self.user.first_name} {self.user.last_name}'
+class PrescriptionItems(BaseModel):
+    prescription = models.ForeignKey(Prescription,on_delete=models.CASCADE,related_name='prescription')
+    drug = models.ForeignKey(Drug,on_delete=models.CASCADE, related_name='drug')
+    dose = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    dispensed = models.BooleanField(default=False)
+    def __str__(self):
+        return f'{self.drug} - {self.dose} - {self.duration}'
