f33f769dbe9c388838e30152e9c0031414be01c7
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-08 15:39:39 +0300

    Create the serializers

---
+++ Lab_Radiology/serializers.py
@@ -0,0 +1,89 @@
+from rest_framework.serializers import ModelSerializer, StringRelatedField
+from rest_framework import serializers
+from Hospital.models import Patient, Doctor
+from Appointments.models import BookedAppointment
+from .models import *
+
+
+
+class ExamsListSerializer(ModelSerializer):
+    class Meta:
+        model = ExamsList
+        fields = '__all__'
+
+
+
+class PatientSerializer(ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = ['id', 'user', 'first_name', 'last_name']
+
+    user = StringRelatedField()
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+
+
+class DoctorSerializer(ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = ['id','user', 'first_name', 'last_name','specialty']
+
+    user = StringRelatedField()
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+
+
+class AppointmentSerializer(ModelSerializer):
+    class Meta:
+        model = BookedAppointment
+        fields = ['id', 'date']
+
+
+
+class ExameRequestSerializer(ModelSerializer):
+
+    exams = ExamsListSerializer(many=True)
+    appointment = AppointmentSerializer()
+    
+    patient = PatientSerializer()
+    doctor = DoctorSerializer()
+
+    class Meta:
+        model = ExamRequest
+        fields = ['id', 'appointment', 'status', 'comment', 'exams', 'patient', 'doctor']
+
+
+
+class CreateExameRequest(ModelSerializer):
+
+    class Meta:
+        model = ExamRequest
+        fields = '__all__'
+
+
+class RadiolgyResultDetailsSerializer(ModelSerializer):
+    class Meta:
+        model = RadiologyResultDetails
+        fields = '__all__'
+
+
+
+
+
+class RadiologyResultSerializer(ModelSerializer):
+    radiology_result = RadiolgyResultDetailsSerializer(many=True)
+    class Meta:
+        model = RadiologyResult
+        fields = ['id', 'exam', 'radiology_result']
+
+
+class CreateRadiologyResult(ModelSerializer):
+    class Meta:
+        model = RadiologyResult
+        fields = '__all__'
+
+
+class TestResultSerializer(ModelSerializer):
+    class Meta:
+        model = TestResult
+        fields = '__all__'
