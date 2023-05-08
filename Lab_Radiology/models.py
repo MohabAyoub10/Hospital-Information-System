69cc2c4f725090250a178d4554a914f4be8b1c22
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-08 15:34:41 +0300

    Updated Result Radiology and Lab models

--- Lab_Radiology/models.py
+++ Lab_Radiology/models.py
@@ -15,7 +15,7 @@ class ExamsList(models.Model):
     price = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
 
     def __str__(self):
-        return self.exam_name
+        return self.name
 
 
 class ExamRequest(models.Model):
@@ -43,26 +43,23 @@ class ExamRequest(models.Model):
 class RadiologyResult(models.Model):
     Request = models.ForeignKey(ExamRequest, on_delete=models.CASCADE,related_name='radiolgy_request')
     exam = models.ForeignKey(ExamsList, on_delete=models.CASCADE, related_name='radiology_exam')
+    dateTime = models.DateTimeField(default=datetime.now)
+    report_file = models.FileField(upload_to='Lab_Radiology/files')
     
 
 class RadiologyResultDetails(models.Model):
     result = models.ForeignKey(RadiologyResult, on_delete=models.CASCADE, related_name='radiology_result')
-    dateTime = models.DateTimeField(default=datetime.now)
     image = models.ImageField(upload_to='Lab_Radiology/files/media')
-    report_file = models.FileField(upload_to='Lab_Radiology/files')
     comment = models.TextField(blank=True)
 
 
 class TestResult(models.Model):
     Request = models.ForeignKey(ExamRequest, on_delete=models.CASCADE,related_name='Lab_request')
     exam = models.ForeignKey(ExamsList, on_delete=models.CASCADE, related_name='Lab_exam')
-
-
-class TestResultDetails(models.Model):
-    result = models.ForeignKey(TestResult, on_delete=models.CASCADE, related_name='radiology_result')
     dateTime = models.DateTimeField(default=datetime.now)
     pdf_result = models.FileField(upload_to='Lab_Radiology/files')
     comment = models.TextField(blank=True)
+
 
 class LabRadiologyStaff(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
