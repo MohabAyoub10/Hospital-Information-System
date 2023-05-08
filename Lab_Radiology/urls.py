e1012f810a12c168646f56e404497810391a1ac4
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-08 15:39:19 +0300

    Create the views classes and add the endpoints in the urls

---
+++ Lab_Radiology/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path , include
+from rest_framework import routers
+from .views import *
+
+
+
+router = routers.DefaultRouter()
+router.register('exams-list', ExamsListViewSet, basename='exams-list')
+router.register('exam-request', ExamRequestViewSet, basename='exam-request')
+router.register('radiology-results', RadiologyResultsViewSet, basename='radiology-results')
+router.register('radiology-result-details', RadiologyResultDetailsViewSet, basename='radiology-result-details')
+router.register('test-result', TestResultViewSet, basename='test-result')
+
+
+urlpatterns = [
+
+    path('', include(router.urls)),
+
+]
--- Lab_Radiology/views.py
+++ Lab_Radiology/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, GenericViewSet, mixins
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import SAFE_METHODS
+from rest_framework import pagination
+from rest_framework import exceptions
+from rest_framework import filters
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+
+
+class ExamsListViewSet(ModelViewSet):
+    queryset = ExamsList.objects.all()
+    serializer_class = ExamsListSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['type']
+    pagination_class = pagination.PageNumberPagination
+
+
+
+
+class ExamRequestViewSet(ModelViewSet):
+    queryset = ExamRequest.objects.prefetch_related('exams').select_related('patient__user','doctor__user').select_related('appointment').all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['status', 'patient', 'doctor','exams__type']
+    pagination_class = pagination.PageNumberPagination
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return ExameRequestSerializer
+        else: return CreateExameRequest
+
+
+
+class RadiologyResultsViewSet(GenericViewSet):
+    queryset = RadiologyResult.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['Request','Request__patient', 'exam']
+    pagination_class = pagination.PageNumberPagination
+
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+           return RadiologyResultSerializer
+        else: return CreateRadiologyResult
+
+
+class RadiologyResultDetailsViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.CreateModelMixin):
+    queryset = RadiologyResultDetails.objects.all()
+    serializer_class = RadiolgyResultDetailsSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['result']
+    search_fields = ['comment']
+    pagination_class = pagination.PageNumberPagination
+
+
+
+
+class TestResultViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.CreateModelMixin):
+    queryset = TestResult.objects.all()
+    serializer_class = TestResultSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['Request__patient','Request', 'exam']
+    pagination_class = pagination.PageNumberPagination
