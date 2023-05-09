0ba04c8afff3c0c678177c83e927b73a13df1855
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-09 19:42:23 +0300

    Create the permissions and add it in the views classes

---
+++ Lab_Radiology/permissions.py
@@ -0,0 +1,44 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class ViewOrCreatOrEditExams(BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.method in SAFE_METHODS:
+                return True
+            return request.user.role in ['radiologist','lab','medical_secretary','admin']
+        return False
+
+
+class CanViewOrEdit(BasePermission):
+    
+    def has_permission(self, request, view):
+            if request.user.is_authenticated:
+                if request.method in SAFE_METHODS and  request.user.role in ['doctor', 'radiologist','lab','patient']:
+                    return True
+                elif request.method in ['POST','DELETE'] and request.user.role in ['doctor']:
+                    return True
+                return request.method in ['PATCH','PUT'] and request.user.role in ['doctor', 'radiologist','lab','receptionist']
+            return False
+
+    
+
+
+class CanViewOrEditOrCreatRadiologyResult(BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.method in SAFE_METHODS and request.user.role in ['doctor', 'radiologist','patient']:
+                return True
+            return request.user.role  == 'radiologist'
+        return False
+
+class CanViewOrEditOrCreatLabResult(BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            if request.method in SAFE_METHODS and request.user.role in ['doctor','lab','patient']:
+                return True
+            return (request.user.role == 'lab')
+        return False
--- Lab_Radiology/views.py
+++ Lab_Radiology/views.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet, mixins
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import SAFE_METHODS
 from rest_framework import pagination
 from rest_framework import exceptions
 from rest_framework import filters
 from .models import *
+from .permissions import *
 from .serializers import *
 
 
@@ -12,6 +13,7 @@ from .serializers import *
 
 class ExamsListViewSet(ModelViewSet):
     queryset = ExamsList.objects.all()
+    permission_classes = [ViewOrCreatOrEditExams]
     serializer_class = ExamsListSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['type']
@@ -21,7 +23,8 @@ class ExamsListViewSet(ModelViewSet):
 
 
 class ExamRequestViewSet(ModelViewSet):
-    queryset = ExamRequest.objects.prefetch_related('exams').select_related('patient__user','doctor__user').select_related('appointment').all()
+    queryset = ExamRequest.objects.prefetch_related('exams').select_related('appointment','patient__user','doctor__user').all()
+    permission_classes = [CanViewOrEdit]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['status', 'patient', 'doctor','exams__type']
     pagination_class = pagination.PageNumberPagination
@@ -33,8 +36,10 @@ class ExamRequestViewSet(ModelViewSet):
 
 
 
-class RadiologyResultsViewSet(GenericViewSet):
-    queryset = RadiologyResult.objects.all()
+
+class RadiologyResultsViewSet(ModelViewSet):
+    queryset = RadiologyResult.objects.select_related('Request','Request__patient__user','Request__doctor__user').select_related('Request__appointment').select_related('exam').prefetch_related('Request__exams').all()
+    permission_classes = [CanViewOrEditOrCreatRadiologyResult]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['Request','Request__patient', 'exam']
     pagination_class = pagination.PageNumberPagination
@@ -47,7 +52,8 @@ class RadiologyResultsViewSet(GenericViewSet):
 
 
 class RadiologyResultDetailsViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.CreateModelMixin):
-    queryset = RadiologyResultDetails.objects.all()
+    queryset = RadiologyResultDetails.objects.select_related('result','result__exam').all()
+    permission_classes = [CanViewOrEditOrCreatRadiologyResult]
     serializer_class = RadiolgyResultDetailsSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['result']
@@ -59,6 +65,7 @@ class RadiologyResultDetailsViewSet(GenericViewSet, mixins.ListModelMixin, mixin
 
 class TestResultViewSet(GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.CreateModelMixin):
     queryset = TestResult.objects.all()
+    permission_classes = [CanViewOrEditOrCreatLabResult]
     serializer_class = TestResultSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['Request__patient','Request', 'exam']
