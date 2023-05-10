2443404f668f336ef6f2b1add541eef2397cca67
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-11 01:33:21 +0300

    update the appointment on delete in the exams request

--- Lab_Radiology/models.py
+++ Lab_Radiology/models.py
@@ -4,19 +4,6 @@ from django.conf import settings
 from Hospital.models import Patient, Doctor
 from Appointments.models import BookedAppointment
 
-class ExamsList(models.Model):
-    Examtypes = [
-        ('Radiology', 'Radiology'),
-        ('Laboratory', 'Laboratory'),
-    ]
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=255, choices=Examtypes)
-    code = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
-
-    def __str__(self):
-        return self.name
-
 
 class ExamRequest(models.Model):
     REQUESTED = 'Requested'
@@ -30,40 +17,43 @@ class ExamRequest(models.Model):
         (COMPLETED, 'Completed'),
         (CANCELLED, 'Cancelled'),
     ]
-    exams = models.ManyToManyField(ExamsList)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_exame')
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='doctor_exame')
-    appointment = models.ForeignKey(BookedAppointment, on_delete=models.CASCADE, related_name='appointment_exame')
-    status = models.CharField(max_length=255, choices=STATUS, default=REQUESTED)
+    appointment = models.ForeignKey(BookedAppointment, on_delete=models.CASCADE, related_name='appointment_exam')
+    status = models.CharField(max_length=10, choices=STATUS, default=REQUESTED)
     dateTime = models.DateTimeField(default=datetime.now)
-    comment = models.TextField(blank=True)
 
 
-
-class RadiologyResult(models.Model):
+class RadiolgyResult(models.Model):
     Request = models.ForeignKey(ExamRequest, on_delete=models.CASCADE,related_name='radiolgy_request')
-    exam = models.ForeignKey(ExamsList, on_delete=models.CASCADE, related_name='radiology_exam')
     dateTime = models.DateTimeField(default=datetime.now)
-    report_file = models.FileField(upload_to='Lab_Radiology/files')
-    
-
-class RadiologyResultDetails(models.Model):
-    result = models.ForeignKey(RadiologyResult, on_delete=models.CASCADE, related_name='radiology_result')
     image = models.ImageField(upload_to='Lab_Radiology/files/media')
+    report_file = models.FileField(upload_to='Lab_Radiology/files')
     comment = models.TextField(blank=True)
 
 
 class TestResult(models.Model):
-    Request = models.ForeignKey(ExamRequest, on_delete=models.CASCADE,related_name='Lab_request')
-    exam = models.ForeignKey(ExamsList, on_delete=models.CASCADE, related_name='Lab_exam')
+    Request = models.ForeignKey(ExamRequest, on_delete=models.CASCADE, related_name='test_request')
     dateTime = models.DateTimeField(default=datetime.now)
-    pdf_result = models.FileField(upload_to='Lab_Radiology/files')
+    result_value = models.DecimalField( max_digits=10, decimal_places=5)
+    unit = models.CharField(max_length=50)
+    min_reference_range = models.DecimalField(max_digits=10, decimal_places=5)
+    max_reference_range = models.DecimalField(max_digits=10, decimal_places=5)
     comment = models.TextField(blank=True)
 
 
-class RadiologyStaff(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class ExaminationList(models.Model):
+    exam_name = models.CharField(max_length=255)
+    exam_type = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.exam_name
 
 
-class LabStaff(models.Model):
+class ExamDetails(models.Model):
+    exam = models.ForeignKey(ExaminationList, null=False, on_delete=models.PROTECT, related_name='exam')
+    request = models.ForeignKey(ExamRequest, null=False, on_delete=models.CASCADE, related_name='request')
+
+
+
+class LabRadiologyStaff(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
