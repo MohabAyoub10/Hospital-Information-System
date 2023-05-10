8437f141af32e96c92ab27c07cab4976a38a9d9e
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-11 01:30:02 +0300

    edit the permissions

--- Appointments/permissions.py
+++ Appointments/permissions.py
@@ -2,19 +2,21 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 def is_admin_or_staff(user):
-    return user.is_authenticated and user.role in ['admin', 'receptionist', 'doctor', 'patient']
+    return user.is_authenticated and user.role in ['admin', 'receptionist', 'doctor']
 
 
 class CanAccess(BasePermission):
 
     def has_permission(self, request, view):
-        return is_admin_or_staff(request.user)
+        return is_admin_or_staff(request.user) and request.user.role == 'patient'
 
 
 class CreateDoctorScheduleOrUpdate(BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
+        if request.method in SAFE_METHODS and is_admin_or_staff(request.user):
+            return True
+        elif request.method in SAFE_METHODS and request.user.role == 'patient' and request.user.is_authenticated:
             return True
         return is_admin_or_staff(request.user) and request.user.role == 'receptionist'
 
@@ -22,6 +24,8 @@ class CreateDoctorScheduleOrUpdate(BasePermission):
 class BookAppointment(BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        return is_admin_or_staff(request.user) and request.user.role in ['patient', 'receptionist']
+        if request.user.is_authenticated:
+            if request.method in SAFE_METHODS and is_admin_or_staff(request.user):
+                 return True
+            return request.user.role in ['patient', 'receptionist']
+        return False
