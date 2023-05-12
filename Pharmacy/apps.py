4411b61c149c0ae3b1cb452634324ec45dc72cf6
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2023-05-12 15:22:18 +0300

    add pharmacist signals

--- Pharmacy/apps.py
+++ Pharmacy/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PharmacyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Pharmacy'
+    def ready(self) -> None:
+        import Pharmacy.signals
