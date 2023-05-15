cb557be9da27726717051e4be4b5fa683ddc9a35
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-15 22:35:38 +0300

    create new serializer to add the examlist

--- Bills/serializer.py
+++ Bills/serializer.py
@@ -5,7 +5,7 @@ from Hospital.models import Patient, Doctor
 from Lab_Radiology.models import ExamRequest
 from Appointments.models import BookedAppointment
 from .models import Bill, InsuranceDetails
-
+from Lab_Radiology.models import ExamRequest, ExamsList
 
 class PatientSerializer(ModelSerializer):
     user = StringRelatedField()
@@ -27,6 +27,19 @@ class DoctorSerializer(ModelSerializer):
         fields = ['id', 'user', 'first_name', 'last_name', 'specialty']
 
 
+class ExamListSerializer(ModelSerializer):
+
+    class Meta:
+        model = ExamsList
+        fields = ['name', 'price']
+
+
+class ExamRequestSerializer(ModelSerializer):
+    exams = ExamListSerializer(many=True)
+
+    class Meta:
+        model = ExamRequest
+        fields = ['id','dateTime', 'status', 'exams']
 class AppointmentsSerializer(ModelSerializer):
     doctor = DoctorSerializer()
 
@@ -41,11 +54,26 @@ class InsuranceDetailsSerializer(ModelSerializer):
     class Meta:
         model = InsuranceDetails
         fields = ['patient', 'company', 'number', 'expairy_date', 'coverage', 'coverage_percentage', 'card']
+        
+
+class CreateInsuranceDetailsSerializer(ModelSerializer):
+    class Meta:
+        model = InsuranceDetails
+        fields = '__all__'
 
 
 class BillsSerializer(serializers.ModelSerializer):
     patient = PatientSerializer()
-
+    insurance = InsuranceDetailsSerializer()
+    appointment = AppointmentsSerializer()
+    examrequest = ExamRequestSerializer()
     class Meta:
         model = Bill
-        fields = ['patient', 'insurance', 'appointment', 'time_date']
+        fields = ['patient', 'appointment', 'examrequest','insurance','time_date', 'total']
+
+
+class CreateBillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bill
+        fields = '__all__'
+        read_only_fields = ['total']
