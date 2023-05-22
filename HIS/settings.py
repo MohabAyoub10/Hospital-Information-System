9d88d9d663b167330f28b116a585605bf7ff022f
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-05-22 22:45:11 +0300

    change the access tken lifetime

--- HIS/settings.py
+++ HIS/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
+   "ACCESS_TOKEN_LIFETIME": timedelta(days=50),
 }
  
 DJOSER = {
