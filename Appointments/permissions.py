b7083ba8295e17787a788f6e0f4897b104bcf14a
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-06-12 00:29:40 +0300

    add the update and delet to the receptionist

--- Appointments/permissions.py
+++ Appointments/permissions.py
@@ -4,21 +4,29 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 def is_admin_or_staff(user):
     return user.is_authenticated and user.role in ['admin', 'receptionist', 'doctor']
 
+
 def is_receptionist(user):
     return user.is_authenticated and user.role == 'receptionist'
+
+
 def is_patient(user):
     return user.is_authenticated and user.role == 'patient'
+
+
 def is_doctor(user):
     return user.is_authenticated and user.role == 'doctor'
+
+
 class CanAccess(BasePermission):
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             if request.method in SAFE_METHODS and request.user.role in ['patient', 'receptionist', 'doctor']:
-                 return True
+                return True
             else:
                 return False
         return False
+
 
 class CreateDoctorScheduleOrUpdate(BasePermission):
 
@@ -27,7 +35,7 @@ class CreateDoctorScheduleOrUpdate(BasePermission):
             return True
         elif request.method in SAFE_METHODS and request.user.role == 'patient' and request.user.is_authenticated:
             return True
-        elif request.method == 'POST' and is_receptionist(request.user):
+        elif request.method in ['POST', 'PUT', 'PATCH'] and is_receptionist(request.user):
             return True
         return False
 
@@ -37,13 +45,13 @@ class BookAppointment(BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             if request.method in SAFE_METHODS and is_admin_or_staff(request.user):
-                 return True
+                return True
             return request.user.role in ['patient', 'receptionist']
         return False
-    
+
 
 class DoctorAppointmentPermission(BasePermission):
-    
-        def has_permission(self, request, view):
-            if request.user.is_authenticated and request.method:
-                return request.user.role in ['doctor', 'receptionist']
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.method:
+            return request.user.role in ['doctor', 'receptionist']
