a587872ab511567bed83168cf1cda922186d8675
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-06-15 03:02:39 +0300

    add udate stock signal

--- Pharmacy/signals.py
+++ Pharmacy/signals.py
@@ -3,12 +3,21 @@ from .models import *
 from django.dispatch import receiver
 from HIS import settings
 
-@receiver(post_save,sender=settings.AUTH_USER_MODEL)
-def create_profile_for_new_user(sender,**kwargs):
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile_for_new_user(sender, **kwargs):
 
     if kwargs['created']:
         user = kwargs['instance']
         if user.role == 'pharmacist':
-           Pharmacist.objects.create(user=user)
-           
-        
+            Pharmacist.objects.create(user=user)
+
+
+@receiver(post_save, sender=Prescription)
+def update_drug_stock(sender, instance, **kwargs):
+    if instance.dispensed_status == 'dispensed':
+        for item in instance.prescription.all():
+            if item.dispensed == True:
+                drug = item.drug
+                drug.stock -= item.amount
+                drug.save()
