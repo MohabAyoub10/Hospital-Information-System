52fc3cad3b058afb8588c909ce5409ed20595422
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-06-17 12:24:02 +0300

    validate the insurance input in the create function

--- Bills/serializer.py
+++ Bills/serializer.py
@@ -7,6 +7,8 @@ from Appointments.models import *
 from .models import Bill, InsuranceDetails
 from Lab_Radiology.models import ExamRequest, ExamsList
 from Pharmacy.models import PrescriptionItems, Prescription
+from pprint import pprint
+
 
 class PatientSerializer(ModelSerializer):
     user = StringRelatedField()
@@ -40,23 +42,30 @@ class ExamRequestSerializer(ModelSerializer):
 
     class Meta:
         model = ExamRequest
-        fields = ['id','dateTime', 'status', 'exams']
+        fields = ['id', 'dateTime', 'status', 'exams']
+
 
 class ScheduleSerializer(ModelSerializer):
     class Meta:
         model = DoctorSchedule
         fields = ['price']
+
+
 class SlotSerializer(ModelSerializer):
     schedule = ScheduleSerializer()
+
     class Meta:
         model = Slot
         fields = ['id', 'start_time', 'schedule']
+
+
 class AppointmentsSerializer(ModelSerializer):
     doctor = DoctorSerializer()
     slot = SlotSerializer()
+
     class Meta:
         model = BookedAppointment
-        fields = ['id', 'date', 'doctor','slot']
+        fields = ['id', 'date', 'doctor', 'slot']
 
 
 class InsuranceDetailsSerializer(ModelSerializer):
@@ -64,33 +73,53 @@ class InsuranceDetailsSerializer(ModelSerializer):
 
     class Meta:
         model = InsuranceDetails
-        fields = ['patient', 'company', 'number', 'expairy_date', 'coverage', 'coverage_percentage', 'card']
-        
+        fields = ['patient', 'company', 'number', 'expairy_date',
+                  'coverage', 'coverage_percentage', 'card']
+
 
 class CreateInsuranceDetailsSerializer(ModelSerializer):
     class Meta:
         model = InsuranceDetails
         fields = '__all__'
 
+    def create(self, validated_data):
+        user = self.context['request'].user
+        role = user.role
+        if role == 'patient':
+            patient_id = user.id
+            if patient_id != validated_data['patient'].id:
+                raise serializers.ValidationError(
+                    "You are not allowed to create insurance details for other patients")
+            else:
+                return InsuranceDetails.objects.create(**validated_data)
+        elif role == 'receptionist':
+            return InsuranceDetails.objects.create(**validated_data)
+
+
 class PrescriptionItemsSerializer(serializers.ModelSerializer):
     drug = serializers.SerializerMethodField()
+
     class Meta:
         model = PrescriptionItems
         fields = ['id', 'drug', 'dispensed']
-    
+
     def get_drug(self, obj):
-        drug= obj.drug 
+        drug = obj.drug
         return {
-        'name': drug.name,
-        'price': drug.price,
+            'name': drug.name,
+            'price': drug.price,
         }
-    
+
+
 class PrescriptionSerializer(serializers.ModelSerializer):
     prescription = PrescriptionItemsSerializer(many=True)
+
     class Meta:
         model = Prescription
-        fields = ['id', 'date', 'notes', 'dispensed_status','dispensed_by', 'prescription']
+        fields = ['id', 'date', 'notes', 'dispensed_status',
+                  'dispensed_by', 'prescription']
         read_only_fields = ['date', 'notes']
+
 
 class BillsSerializer(serializers.ModelSerializer):
     patient = PatientSerializer()
@@ -99,9 +128,11 @@ class BillsSerializer(serializers.ModelSerializer):
     radiology_request = ExamRequestSerializer()
     lab_request = ExamRequestSerializer()
     prescription = PrescriptionSerializer()
+
     class Meta:
         model = Bill
-        fields = ['id','patient', 'appointment', 'radiology_request','lab_request','prescription', 'insurance','time_date', 'total']
+        fields = ['id', 'patient', 'appointment', 'radiology_request',
+                  'lab_request', 'prescription', 'insurance', 'time_date', 'total']
 
 
 class CreateBillsSerializer(serializers.ModelSerializer):
