fb2fb1bbad0d852ca95cb638c140f4c6da4460dd
Mo.ayoub <64101526+MohabAyoub10@users.noreply.github.com>
2023-07-08 20:47:47 +0300

    make the address field is nullable

--- Records/models.py
+++ Records/models.py
@@ -35,7 +35,7 @@ class EmergencyContact(models.Model):
     relative_relation = models.CharField(max_length=12, choices=relatives)
     national_id = models.CharField(max_length=14, unique=1)
     address = models.ForeignKey(
-        Address, related_name='emergency_contact_address', on_delete=models.CASCADE, null=1, blank=1)
+        Address, related_name='emergency_contact_address', on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         try:
